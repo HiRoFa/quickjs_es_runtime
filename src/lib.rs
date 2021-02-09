509f02077e5809dfd0fd3a9d312d57bf2f136079
Andries Hiemstra <andries@fam-hiemstra.nl>
2021-02-09 10:35:33 +0100

    docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,48 @@
-#[allow(unused_imports)]
+//! # quickjs_runtime
+//! This crate is made up of two main parts:
+//! * thread-safe utils and wrappers
+//!   you can call these from any thread, all logic is directed to a single worker-thread which talks to the quickjs API
+//! * quickjs bindings and utils
+//!   these talk to the quickjs API directly and need to run in the same thread as the Runtime
+//!
+//! | Thread safe | Runtime Thread-local |
+//! | --- | --- |
+//! | [EsRuntime](https://hirofa.github.io/quickjs_es_runtime/quickjs_runtime/esruntime/struct.EsRuntime.html) | [QuickJsRuntime](https://hirofa.github.io/quickjs_es_runtime/quickjs_runtime/quickjsruntime/struct.QuickJsRuntime.html) |
+//! | [EsValueFacade](https://hirofa.github.io/quickjs_es_runtime/quickjs_runtime/esvalue/struct.EsValueFacade.html) | [JSValueRef](https://hirofa.github.io/quickjs_es_runtime/quickjs_runtime/valueref/struct.JSValueRef.html) |
+//!
+//! ## Doing something in the runtime worker thread
+//!
+//! You always start with building a new ESRuntime
+//!
+//! ```dontrun
+//! use quickjs_runtime::esruntimebuilder::EsRuntimeBuilder;
+//! let rt = EsRuntimeBuilder::new().build();
+//! ```
+//!
+//! [EsRuntime](https://hirofa.github.io/quickjs_es_runtime/quickjs_runtime/esruntime/struct.EsRuntime.html) has plenty public methods you can check out but one the things you'l need to understand is how to communicate to the actual QuickJsRuntime
+//! This is done by adding a job to the EventQueue of the EsRuntime
+//!
+//! ```dontrun
+//! use quickjs_runtime::quickjsruntime::QuickJsRuntime;
+//! rt.add_to_event_queue(|q_js_rt: &QuickJsRuntime| {
+//!    // this will run in the Worker thread, here we can use the quickjs API
+//! });
+//! ```
+//!
+//! In order to do something and get the result you can use the sync variant
+//! ```dontrun
+//! use quickjs_runtime::quickjsruntime::QuickJsRuntime;
+//! let res = rt.add_to_event_queue_sync(|q_js_rt: &QuickJsRuntime| {
+//!    // this will run in the Worker thread, here we can use the quickjs API
+//!    return 1;
+//! });
+//! ```
+//!
+//! For more details and examples please explore the packages below
+
 #[macro_use]
 extern crate lazy_static;
 
-/// # quickjs_runtime
-/// This crate is made up of two main parts:
-/// * thread-safe utils and wrappers
-///   you can call these from any thread, all logic is directed to a single worker-thread which talks to the quickjs API
-/// * quickjs bindings and utils
-///   these talk to the quickjs API directly and need to run in the same thread as the Runtime
-///
-///  | Thread safe | Runtime Thread-local |
-///  | --- | --- |
-///  | [EsRuntime](https://hirofa.github.io/quickjs_es_runtime/quickjs_runtime/esruntime/struct.EsRuntime.html) | [QuickJsRuntime](https://hirofa.github.io/quickjs_es_runtime/quickjs_runtime/quickjsruntime/struct.QuickJsRuntime.html) |
-///  | [EsValueFacade](https://hirofa.github.io/quickjs_es_runtime/quickjs_runtime/esvalue/struct.EsValueFacade.html) | [JSValueRef](https://hirofa.github.io/quickjs_es_runtime/quickjs_runtime/valueref/struct.JSValueRef.html) |
-///
-/// ## Doing something in the runtime worker thread
-///
-/// You always start with building a new ESRuntime
-///
-/// ```ignore
-/// use quickjs_runtime::esruntimebuilder::EsRuntimeBuilder;
-/// let rt = EsRuntimeBuilder::new().build();
-/// ```
-///
-/// [EsRuntime](https://hirofa.github.io/quickjs_es_runtime/quickjs_runtime/esruntime/struct.EsRuntime.html) has plenty public methods you can check out but one the things you'l need to understand is how to communicate to the actual QuickJsRuntime
-/// This is done by adding a job to the EventQueue of the EsRuntime
-///
-/// ```ignore
-/// rt.add_to_event_queue(|q_js_rt: &QuickJsRuntime| {
-///    // this will run in the Worker thread, here we can use the quickjs API
-/// });
-/// ```
-///
-/// In order to do something and get the result you can use the sync variant
-/// ```ignore
-/// let res = rt.add_to_event_queue_sync(|q_js_rt: &QuickJsRuntime| {
-///    // this will run in the Worker thread, here we can use the quickjs API
-///    return 1;
-/// });
-/// ```
-///
 mod droppable_value;
 pub mod eserror;
 pub mod esruntime;
