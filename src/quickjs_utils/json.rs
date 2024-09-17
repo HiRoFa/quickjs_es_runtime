0253bcea5c2f345ff7fc6274233d108305fa8f2f
andries@fam-hiemstra.nl <andries@fam-hiemstra.nl>
2024-09-17 08:51:12 +0200

    change to json test

--- src/quickjs_utils/json.rs
+++ src/quickjs_utils/json.rs
@@ -157,13 +157,13 @@ pub mod tests {
             .unwrap();
             let str_res = json::stringify_q(q_ctx, &obj, None).ok().unwrap();
             assert_eq!(str_res.get_ref_count(), 1);
-            let json = str_res.to_str().ok().unwrap();
+            let json = str_res.to_string().ok().unwrap();
             assert_eq!(json, "{\"a\":532,\"b\":true,\"c\":\"abcdË\"}");
 
-            let obj2 = json::parse_q(q_ctx, json).ok().unwrap();
+            let obj2 = parse_q(q_ctx, json.as_str()).ok().unwrap();
 
             let prop_c = objects::get_property_q(q_ctx, &obj2, "c").ok().unwrap();
-            assert_eq!("abcdË", prop_c.to_str().ok().unwrap());
+            assert_eq!("abcdË", prop_c.to_string().ok().unwrap());
         });
     }
 
